3cca9d6119b942614aeadaccf652f0d313845f88
NFT2626 <96617076+NFT2626@users.noreply.github.com>
2022-01-09 13:55:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -432,9 +432,9 @@
     let app_gen, apparata;
     const number_of_apparata = 15;
     const scale = 5;
-    const shuffle = 90;
+    const shuffle = 120;
     let tick = 0;
-    const final_frame_duration = 150;
+    const final_frame_duration = 130;
     let symmetric_assembly = true;
     const movement_length = 0.8;
     const row_length = 6;
